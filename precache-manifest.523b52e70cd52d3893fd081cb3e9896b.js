9654b5366468ad5999b7d50902594969681600bd
Shannon Roberts <smrobertsllc@gmail.com>
2020-02-29 22:04:35 -0500

    Updates

---
+++ precache-manifest.523b52e70cd52d3893fd081cb3e9896b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7f5d5c07d3631c79ba7c54942a09e8b",
+    "url": "/ReactClickyGame_Memory/index.html"
+  },
+  {
+    "revision": "e6e6f72d76666646bdd2",
+    "url": "/ReactClickyGame_Memory/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "d786d19eeae36dbc1010",
+    "url": "/ReactClickyGame_Memory/static/js/2.47544821.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ReactClickyGame_Memory/static/js/2.47544821.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6e6f72d76666646bdd2",
+    "url": "/ReactClickyGame_Memory/static/js/main.681b22d4.chunk.js"
+  },
+  {
+    "revision": "c8cbabcd7c8646cab77a",
+    "url": "/ReactClickyGame_Memory/static/js/runtime-main.92da5168.js"
+  }
+]);
